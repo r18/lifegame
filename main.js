da6ca24e4567108b3229e57c73f42089b0b250b6
r18 <twkgr424@yahoo.co.jp>
2014-03-11 15:45:34 +0900

    Add function drawBoard

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
 function main(){
-  console.log("hello world");
-
+  drawBoard(4,4);
 }
 
 function drawBoard(width,height){
-
+  var table = document.createElement('tbody');
+  for(var y = 0;y < height; y++){
+    var tr = document.createElement('tr');
+    for(var x = 0;x < width; x++){
+     var td = document.createElement('td');
+     td.id = x+y*width;
+     td.style.backgroundColor = x%2==1?"black":"red";
+     tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+  document.getElementById("board").appendChild(table);
+  
 }
